783b33a5e94a1a29fd205d7c63a1d0bee8f831e8
r0beer <r0beersosa@gmail.com>
2022-12-10 18:29:21 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'hola';
 const nuevoNum = '3';
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = 'true';
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
